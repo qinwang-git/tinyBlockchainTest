0a3aec6ff34ec02b4e9c4de4f470efcf9a7e7fde
shiningmyheart <450618879@qq.com>
2018-11-21 22:59:07 +1100

    feat:

--- BLC/Blockchain.go
+++ BLC/Blockchain.go
@@ -4,14 +4,17 @@ type BlockChain struct {
 	Blocks []*Block
 }
 
+// 创建带有创世区块的链
 func CreateBlockChainWithGenesisBlock(data string) *BlockChain {
-
+	//创建创世区块
 	genesisBlock := CreateGenesisBlock(data)
+	//返回区块链对象
 	return &BlockChain{[]*Block{genesisBlock}}
 }
 
+// 添加新区块入链
 func (bc *BlockChain) AddBlockToBlockChain(data string, height int64, prevHash []byte) {
-
+	//创建新区块
 	newBlock := NewBlock(data, prevHash, height)
 	//将newBlock添加切片至Block后面
 	bc.Blocks = append(bc.Blocks, newBlock)
